279543654114db7343bd6ac3653abe724ede1b79
zahrank21 <zahrank@gmail.com>
2017-12-30 11:24:47 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr,str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+ arr.pop
+end
+
+# def using_shift(arr)
+#   arr.shift
+# end
+#
+# def using_concat(arr,str)
+#
+# end
+#
+# def using_uniq(arr)
+#
+# end
+#
+# def using_insert(arr)
